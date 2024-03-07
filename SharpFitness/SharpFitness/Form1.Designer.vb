8f13be8a375f1bed6f3e98b65758c5382b93efc4
macenoq-stack <139469695+macenoq-stack@users.noreply.github.com>
2024-03-07 09:48:40 +0300

    Add files via upload

---
+++ SharpFitness/SharpFitness/Form1.Designer.vb
@@ -0,0 +1,336 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class PromotionForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(PromotionForm))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.LineShape1 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.departmentGroupBox = New System.Windows.Forms.GroupBox()
+        Me.personalTrainingRadioButton = New System.Windows.Forms.RadioButton()
+        Me.membershipRadioButton = New System.Windows.Forms.RadioButton()
+        Me.juiceBarRadioButton = New System.Windows.Forms.RadioButton()
+        Me.equipmentRadioButton = New System.Windows.Forms.RadioButton()
+        Me.clothingRadioButton = New System.Windows.Forms.RadioButton()
+        Me.imageVisibleCheckBox = New System.Windows.Forms.CheckBox()
+        Me.nameLabel = New System.Windows.Forms.Label()
+        Me.memberIDLabel = New System.Windows.Forms.Label()
+        Me.nameTextBox = New System.Windows.Forms.TextBox()
+        Me.promotionsTextBox = New System.Windows.Forms.TextBox()
+        Me.welcomeRichTextBox = New System.Windows.Forms.RichTextBox()
+        Me.signInButton = New System.Windows.Forms.Button()
+        Me.printButton = New System.Windows.Forms.Button()
+        Me.exitButton = New System.Windows.Forms.Button()
+        Me.memberIDMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Print = New System.Windows.Forms.PrintDialog()
+        Me.PrintPreviewDialog1 = New System.Windows.Forms.PrintPreviewDialog()
+        Me.departmentPictureBox = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.departmentGroupBox.SuspendLayout()
+        CType(Me.departmentPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Blue
+        Me.Label1.Location = New System.Drawing.Point(49, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(232, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Look Sharp Fitness Center"
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.LineShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(421, 439)
+        Me.ShapeContainer1.TabIndex = 1
+        Me.ShapeContainer1.TabStop = False
+        '
+        'LineShape1
+        '
+        Me.LineShape1.BorderColor = System.Drawing.Color.DarkBlue
+        Me.LineShape1.BorderWidth = 5
+        Me.LineShape1.Name = "LineShape1"
+        Me.LineShape1.X1 = 2
+        Me.LineShape1.X2 = 414
+        Me.LineShape1.Y1 = 35
+        Me.LineShape1.Y2 = 34
+        '
+        'departmentGroupBox
+        '
+        Me.departmentGroupBox.Controls.Add(Me.Label2)
+        Me.departmentGroupBox.Controls.Add(Me.personalTrainingRadioButton)
+        Me.departmentGroupBox.Controls.Add(Me.membershipRadioButton)
+        Me.departmentGroupBox.Controls.Add(Me.juiceBarRadioButton)
+        Me.departmentGroupBox.Controls.Add(Me.equipmentRadioButton)
+        Me.departmentGroupBox.Controls.Add(Me.clothingRadioButton)
+        Me.departmentGroupBox.Location = New System.Drawing.Point(3, 43)
+        Me.departmentGroupBox.Name = "departmentGroupBox"
+        Me.departmentGroupBox.Size = New System.Drawing.Size(200, 170)
+        Me.departmentGroupBox.TabIndex = 2
+        Me.departmentGroupBox.TabStop = False
+        Me.departmentGroupBox.Text = "Department"
+        '
+        'personalTrainingRadioButton
+        '
+        Me.personalTrainingRadioButton.AutoSize = True
+        Me.personalTrainingRadioButton.Location = New System.Drawing.Point(9, 147)
+        Me.personalTrainingRadioButton.Name = "personalTrainingRadioButton"
+        Me.personalTrainingRadioButton.Size = New System.Drawing.Size(107, 17)
+        Me.personalTrainingRadioButton.TabIndex = 4
+        Me.personalTrainingRadioButton.TabStop = True
+        Me.personalTrainingRadioButton.Text = "Personal &Training"
+        Me.personalTrainingRadioButton.UseVisualStyleBackColor = True
+        '
+        'membershipRadioButton
+        '
+        Me.membershipRadioButton.AutoSize = True
+        Me.membershipRadioButton.Location = New System.Drawing.Point(9, 124)
+        Me.membershipRadioButton.Name = "membershipRadioButton"
+        Me.membershipRadioButton.Size = New System.Drawing.Size(82, 17)
+        Me.membershipRadioButton.TabIndex = 3
+        Me.membershipRadioButton.TabStop = True
+        Me.membershipRadioButton.Text = "&Membership"
+        Me.membershipRadioButton.UseVisualStyleBackColor = True
+        '
+        'juiceBarRadioButton
+        '
+        Me.juiceBarRadioButton.AutoSize = True
+        Me.juiceBarRadioButton.Location = New System.Drawing.Point(9, 93)
+        Me.juiceBarRadioButton.Name = "juiceBarRadioButton"
+        Me.juiceBarRadioButton.Size = New System.Drawing.Size(69, 17)
+        Me.juiceBarRadioButton.TabIndex = 2
+        Me.juiceBarRadioButton.TabStop = True
+        Me.juiceBarRadioButton.Text = "&Juice Bar"
+        Me.juiceBarRadioButton.UseVisualStyleBackColor = True
+        '
+        'equipmentRadioButton
+        '
+        Me.equipmentRadioButton.AutoSize = True
+        Me.equipmentRadioButton.Location = New System.Drawing.Point(9, 70)
+        Me.equipmentRadioButton.Name = "equipmentRadioButton"
+        Me.equipmentRadioButton.Size = New System.Drawing.Size(137, 17)
+        Me.equipmentRadioButton.TabIndex = 1
+        Me.equipmentRadioButton.TabStop = True
+        Me.equipmentRadioButton.Text = "&Equipment/Accessories"
+        Me.equipmentRadioButton.UseVisualStyleBackColor = True
+        '
+        'clothingRadioButton
+        '
+        Me.clothingRadioButton.AutoSize = True
+        Me.clothingRadioButton.Location = New System.Drawing.Point(9, 47)
+        Me.clothingRadioButton.Name = "clothingRadioButton"
+        Me.clothingRadioButton.Size = New System.Drawing.Size(63, 17)
+        Me.clothingRadioButton.TabIndex = 0
+        Me.clothingRadioButton.TabStop = True
+        Me.clothingRadioButton.Text = "&Clothing"
+        Me.clothingRadioButton.UseVisualStyleBackColor = True
+        '
+        'imageVisibleCheckBox
+        '
+        Me.imageVisibleCheckBox.AutoSize = True
+        Me.imageVisibleCheckBox.Location = New System.Drawing.Point(12, 410)
+        Me.imageVisibleCheckBox.Name = "imageVisibleCheckBox"
+        Me.imageVisibleCheckBox.Size = New System.Drawing.Size(88, 17)
+        Me.imageVisibleCheckBox.TabIndex = 6
+        Me.imageVisibleCheckBox.Text = "Image &Visible"
+        Me.imageVisibleCheckBox.UseVisualStyleBackColor = True
+        '
+        'nameLabel
+        '
+        Me.nameLabel.AutoSize = True
+        Me.nameLabel.Location = New System.Drawing.Point(242, 62)
+        Me.nameLabel.Name = "nameLabel"
+        Me.nameLabel.Size = New System.Drawing.Size(35, 13)
+        Me.nameLabel.TabIndex = 7
+        Me.nameLabel.Text = "&Name"
+        '
+        'memberIDLabel
+        '
+        Me.memberIDLabel.AutoSize = True
+        Me.memberIDLabel.Location = New System.Drawing.Point(231, 101)
+        Me.memberIDLabel.Name = "memberIDLabel"
+        Me.memberIDLabel.Size = New System.Drawing.Size(59, 13)
+        Me.memberIDLabel.TabIndex = 8
+        Me.memberIDLabel.Text = "Member &ID"
+        '
+        'nameTextBox
+        '
+        Me.nameTextBox.Location = New System.Drawing.Point(299, 61)
+        Me.nameTextBox.Name = "nameTextBox"
+        Me.nameTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.nameTextBox.TabIndex = 9
+        '
+        'promotionsTextBox
+        '
+        Me.promotionsTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.promotionsTextBox.Location = New System.Drawing.Point(253, 228)
+        Me.promotionsTextBox.Name = "promotionsTextBox"
+        Me.promotionsTextBox.Size = New System.Drawing.Size(146, 20)
+        Me.promotionsTextBox.TabIndex = 12
+        Me.promotionsTextBox.TabStop = False
+        Me.promotionsTextBox.Visible = False
+        '
+        'welcomeRichTextBox
+        '
+        Me.welcomeRichTextBox.Location = New System.Drawing.Point(253, 135)
+        Me.welcomeRichTextBox.Name = "welcomeRichTextBox"
+        Me.welcomeRichTextBox.Size = New System.Drawing.Size(146, 78)
+        Me.welcomeRichTextBox.TabIndex = 13
+        Me.welcomeRichTextBox.Text = ""
+        Me.welcomeRichTextBox.Visible = False
+        '
+        'signInButton
+        '
+        Me.signInButton.Location = New System.Drawing.Point(324, 254)
+        Me.signInButton.Name = "signInButton"
+        Me.signInButton.Size = New System.Drawing.Size(75, 23)
+        Me.signInButton.TabIndex = 14
+        Me.signInButton.Text = "&Sign In"
+        Me.signInButton.UseVisualStyleBackColor = True
+        '
+        'printButton
+        '
+        Me.printButton.Location = New System.Drawing.Point(324, 341)
+        Me.printButton.Name = "printButton"
+        Me.printButton.Size = New System.Drawing.Size(75, 23)
+        Me.printButton.TabIndex = 15
+        Me.printButton.Text = "&Print"
+        Me.printButton.UseVisualStyleBackColor = True
+        '
+        'exitButton
+        '
+        Me.exitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.exitButton.Location = New System.Drawing.Point(324, 395)
+        Me.exitButton.Name = "exitButton"
+        Me.exitButton.Size = New System.Drawing.Size(75, 23)
+        Me.exitButton.TabIndex = 16
+        Me.exitButton.Text = "E&xit"
+        Me.exitButton.UseVisualStyleBackColor = True
+        '
+        'memberIDMaskedTextBox
+        '
+        Me.memberIDMaskedTextBox.Location = New System.Drawing.Point(296, 101)
+        Me.memberIDMaskedTextBox.Mask = "0000"
+        Me.memberIDMaskedTextBox.Name = "memberIDMaskedTextBox"
+        Me.memberIDMaskedTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.memberIDMaskedTextBox.TabIndex = 17
+        Me.ToolTip1.SetToolTip(Me.memberIDMaskedTextBox, "Your 5 digit member number")
+        '
+        'Print
+        '
+        Me.Print.UseEXDialog = True
+        '
+        'PrintPreviewDialog1
+        '
+        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog1.Enabled = True
+        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        Me.PrintPreviewDialog1.Visible = False
+        '
+        'departmentPictureBox
+        '
+        Me.departmentPictureBox.Location = New System.Drawing.Point(9, 227)
+        Me.departmentPictureBox.Name = "departmentPictureBox"
+        Me.departmentPictureBox.Size = New System.Drawing.Size(224, 177)
+        Me.departmentPictureBox.TabIndex = 5
+        Me.departmentPictureBox.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Uighur", 11.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.DarkSlateGray
+        Me.Label2.Location = New System.Drawing.Point(5, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(146, 22)
+        Me.Label2.TabIndex = 18
+        Me.Label2.Text = "Select Department of Your choice"
+        '
+        'PromotionForm
+        '
+        Me.AcceptButton = Me.signInButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.exitButton
+        Me.ClientSize = New System.Drawing.Size(421, 439)
+        Me.Controls.Add(Me.memberIDMaskedTextBox)
+        Me.Controls.Add(Me.exitButton)
+        Me.Controls.Add(Me.printButton)
+        Me.Controls.Add(Me.signInButton)
+        Me.Controls.Add(Me.welcomeRichTextBox)
+        Me.Controls.Add(Me.promotionsTextBox)
+        Me.Controls.Add(Me.nameTextBox)
+        Me.Controls.Add(Me.memberIDLabel)
+        Me.Controls.Add(Me.nameLabel)
+        Me.Controls.Add(Me.imageVisibleCheckBox)
+        Me.Controls.Add(Me.departmentPictureBox)
+        Me.Controls.Add(Me.departmentGroupBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.Name = "PromotionForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.ToolTip1.SetToolTip(Me, "Your 5 digit member number.")
+        Me.departmentGroupBox.ResumeLayout(False)
+        Me.departmentGroupBox.PerformLayout()
+        CType(Me.departmentPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
+    Friend WithEvents LineShape1 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents departmentGroupBox As System.Windows.Forms.GroupBox
+    Friend WithEvents personalTrainingRadioButton As System.Windows.Forms.RadioButton
+    Friend WithEvents membershipRadioButton As System.Windows.Forms.RadioButton
+    Friend WithEvents juiceBarRadioButton As System.Windows.Forms.RadioButton
+    Friend WithEvents equipmentRadioButton As System.Windows.Forms.RadioButton
+    Friend WithEvents clothingRadioButton As System.Windows.Forms.RadioButton
+    Friend WithEvents imageVisibleCheckBox As System.Windows.Forms.CheckBox
+    Friend WithEvents nameLabel As System.Windows.Forms.Label
+    Friend WithEvents memberIDLabel As System.Windows.Forms.Label
+    Friend WithEvents nameTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents promotionsTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents welcomeRichTextBox As System.Windows.Forms.RichTextBox
+    Friend WithEvents signInButton As System.Windows.Forms.Button
+    Friend WithEvents printButton As System.Windows.Forms.Button
+    Friend WithEvents exitButton As System.Windows.Forms.Button
+    Friend WithEvents memberIDMaskedTextBox As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+    Friend WithEvents Print As System.Windows.Forms.PrintDialog
+    Friend WithEvents PrintPreviewDialog1 As System.Windows.Forms.PrintPreviewDialog
+    Friend WithEvents departmentPictureBox As System.Windows.Forms.PictureBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ SharpFitness/SharpFitness/Form1.vb
@@ -0,0 +1,162 @@
+﻿'project : CH04HandsOn
+'programmer : Enoq
+'Date: 27/02/2024
+'Description : the project prompt picture from it's department with relevant member name and membership ID  
+
+
+
+Imports System.Drawing.Printing
+Public Class PromotionForm
+
+    Private printDocument As New PrintDocument()
+    Private Shared Property PrintAction As PrintAction
+
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles clothingRadioButton.CheckedChanged
+        'Display the clothing image and show the special
+        departmentPictureBox.Image = Image.FromFile("C:\Users\Students\Desktop\stella\enzo.png")
+        promotionsTextBox.Text = "30% off clearance items."
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub signInButton_Click(sender As Object, e As EventArgs) Handles signInButton.Click
+
+        ' Check if name and member ID are inputted
+        If String.IsNullOrWhiteSpace(nameTextBox.Text) Or String.IsNullOrWhiteSpace(memberIDMaskedTextBox.Text) Then
+            MessageBox.Show("Please enter your name and member ID before signing in.", "Missing Information", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return ' Exit the event handler if name or member ID is missing
+        End If
+        'Display the specials, set the visibility of the controls.
+        welcomeRichTextBox.Text = "Welcome Member #" + memberIDMaskedTextBox.Text + Environment.NewLine + nameTextBox.Text
+
+        'Set visibility properties.
+        memberIDLabel.Visible = False
+        memberIDMaskedTextBox.Visible = False
+        nameLabel.Visible = False
+        nameTextBox.Visible = False
+        welcomeRichTextBox.Visible = True
+        promotionsTextBox.Visible = True
+        imageVisibleCheckBox.Visible = True
+        departmentPictureBox.Visible = False
+
+        'Enable the radio buttons.
+        departmentGroupBox.Enabled = True
+
+    End Sub
+
+    Private Sub printButton_Click(sender As Object, e As EventArgs) Handles printButton.Click
+
+        ' Check if name and member ID are inputted
+        If String.IsNullOrWhiteSpace(nameTextBox.Text) Or String.IsNullOrWhiteSpace(memberIDMaskedTextBox.Text) Then
+            MessageBox.Show("Please enter your name and member ID before printing.", "Missing Information", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return ' Exit the event handler if name or member ID is missing
+        End If
+
+        ' Attach event handler for printing
+        AddHandler PrintDocument.PrintPage, AddressOf PrintDocument_PrintPage
+
+        ' Show print dialog
+        Dim printDialog As New PrintDialog()
+        printDialog.Document = PrintDocument
+
+        If printDialog.ShowDialog() = DialogResult.OK Then
+            PrintDocument.Print()
+        End If
+
+        ' Remove event handler after printing
+        RemoveHandler PrintDocument.PrintPage, AddressOf PrintDocument_PrintPage
+
+    End Sub
+
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+        'End the project.
+        Me.Close()
+    End Sub
+
+    Private Sub equipmentRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles equipmentRadioButton.CheckedChanged
+        'Display the equipment image and show the special
+        departmentPictureBox.Image = Image.FromFile("C:\Users\Students\Desktop\stella\tools.png")
+        promotionsTextBox.Text = "25% off clearance items."
+    End Sub
+
+    Private Sub membershipRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles membershipRadioButton.CheckedChanged
+        'Display the membership image and show the special
+        departmentPictureBox.Image = Image.FromFile("C:\Users\Students\Desktop\stella\membership.png")
+        promotionsTextBox.Text = "Free Personal Trainer for 1st month."
+    End Sub
+
+    Private Sub personalTrainingRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles personalTrainingRadioButton.CheckedChanged
+        'Display the clothing image and show the special
+        departmentPictureBox.Image = Image.FromFile("C:\Users\Students\Desktop\stella\trainer.png")
+        promotionsTextBox.Text = "3 free sessions with membership renewal."
+    End Sub
+
+    Private Sub imageVisibleCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles imageVisibleCheckBox.CheckedChanged
+        'set the visibility of the department images
+        departmentPictureBox.Visible = imageVisibleCheckBox.Checked
+    End Sub
+
+    Private Sub juiceBarRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles juiceBarRadioButton.CheckedChanged
+        'Display the juice image and show the special
+        departmentPictureBox.Image = Image.FromFile("C:\Users\Students\Desktop\stella\juice.png")
+        promotionsTextBox.Text = "free serving of WheatBerry Shake."
+    End Sub
+
+    Private Sub PrintDocument_PrintPage(sender As Object, e As PrintPageEventArgs)
+        ' Get the size of the print area
+        Dim printAreaWidth As Integer = e.MarginBounds.Width
+        Dim printAreaHeight As Integer = e.MarginBounds.Height
+
+        ' Get the size of the image
+        Dim imageWidth As Integer = departmentPictureBox.Image.Width
+        Dim imageHeight As Integer = departmentPictureBox.Image.Height
+
+        ' Calculate the scaling factor
+        Dim scaleFactorX As Single = printAreaWidth / imageWidth
+        Dim scaleFactorY As Single = printAreaHeight / imageHeight
+        Dim scaleFactor As Single = Math.Min(scaleFactorX, scaleFactorY)
+
+        ' Calculate the size of the scaled image
+        Dim scaledWidth As Integer = CInt(imageWidth * scaleFactor)
+        Dim scaledHeight As Integer = CInt(imageHeight * scaleFactor)
+
+        ' Calculate the position to draw the image
+        Dim x As Integer = e.MarginBounds.Left + (printAreaWidth - scaledWidth) / 2
+        Dim y As Integer = e.MarginBounds.Top + (printAreaHeight - scaledHeight) / 2
+
+        ' Draw the scaled image on the print page
+        e.Graphics.DrawImage(departmentPictureBox.Image, x, y, scaledWidth, scaledHeight)
+
+        ' Print the name and member ID
+        Dim font As New Font("Arial", 12)
+        Dim brush As New SolidBrush(Color.Black)
+        Dim nameMemberID As String = "Name: " & nameTextBox.Text & ", Member ID: " & memberIDMaskedTextBox.Text
+
+        Dim stringSize As SizeF = e.Graphics.MeasureString(nameMemberID, font)
+
+        ' Calculate the position to print the text
+        Dim textX As Single = x
+        Dim textY As Single = y + scaledHeight + 10
+
+        ' Draw the text on the print page
+        e.Graphics.DrawString(nameMemberID, font, brush, textX, textY)
+
+        ' Print the promotion text
+        Dim promotionText As String = "Promotion: " & promotionsTextBox.Text
+        textY += stringSize.Height + 5 ' Adjust vertical position
+        e.Graphics.DrawString(promotionText, font, brush, textX, textY)
+
+
+    End Sub
+
+
+    Private Sub departmentPictureBox_Click(sender As Object, e As EventArgs) Handles departmentPictureBox.Click
+        ' Set the SizeMode property of the picture box to Zoom
+        departmentPictureBox.SizeMode = PictureBoxSizeMode.Zoom
+    End Sub
+
+  
+End Class
